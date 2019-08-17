d48db4e1434ff66df21dae5cb41c5b54653e82f3
Hibikine Kage <goodsmile314159@gmail.com>
2019-08-17 17:31:10 +0900

    hoge

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject my-stuff "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://github.com/HibikineKage/my-stuff"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "2.0.0"]]
   :main ^:skip-aot my-stuff.core
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :aot [my-stuff.core]
+  :profiles {:dev {:dependencies [[ring/ring-devel "1.4.0"]]}})
--- src/my_stuff/core.clj
+++ src/my_stuff/core.clj
@@ -1,7 +1,6 @@
 (ns my-stuff.core
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (println "Welcome to my project! These are your args:" args))
+
